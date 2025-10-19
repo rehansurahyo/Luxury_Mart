d9f136af88b2507f45a6e5cd1bf0bc52922dd312
rehansurahyo <rehansurahyo@gmail.com>
2025-10-19 21:15:46 +0500

    this is my 4th commit

---
+++ lib/repository/Screens/Bottom navigation/bottomNavScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:luxury_mart/repository/Screens/cart/cartScreen.dart';
+import 'package:luxury_mart/repository/Screens/category/categoryScreen.dart';
+import 'package:luxury_mart/repository/Screens/home/homeScreen.dart';
+import 'package:luxury_mart/repository/Screens/print/printScreen.dart';
+import 'package:iconsax/iconsax.dart';
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavsSreenState();
+}
+
+class _BottomNavsSreenState extends State<BottomNavScreen> {
+  int currentIndex = 0;
+  List<Widget>pages =[
+    Homescreen(),
+    CartScreen(),
+    CategoryScreen(),
+    PrintScreen(),
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: pages,
+      ),
+
+      bottomNavigationBar: BottomNavigationBar( items: [
+        BottomNavigationBarItem(icon: Icon(Iconsax.home,color: Colors.black,),label: 'Home',),
+        BottomNavigationBarItem(icon: Icon(Iconsax.shopping_cart,color: Colors.black,),label: 'Cart'),
+        BottomNavigationBarItem(icon: Icon(Iconsax.category,color: Colors.black,),label: 'Categories'),
+        BottomNavigationBarItem(icon: Icon(Iconsax.printer,color: Colors.black,),label: 'Print'),
+      ],type: BottomNavigationBarType.fixed,currentIndex: currentIndex,onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },),
+
+
+    );
+  }
+}
--- lib/repository/Screens/Login/loginscreen.dart
+++ lib/repository/Screens/Login/loginscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:luxury_mart/repository/Screens/Bottom%20navigation/bottomNavScreen.dart';
 import 'package:luxury_mart/repository/widgets/uihelper.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -45,11 +46,12 @@ class LoginScreen extends StatelessWidget {
                           SizedBox(height: 5),
                           UiHelper.CustomText(text: '03172208626', color: Color(0XFF9C9C9C), fontweight: FontWeight.bold, fontsize: 14,fontFamily: 'bold'),
                           SizedBox(height: 20),
-                          SizedBox(height: 48,width: 255,child: ElevatedButton(onPressed: (){}, style:ElevatedButton.styleFrom(backgroundColor: Color(0XFFE23744),shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),child: UiHelper.CustomText(text: 'Login', color: Colors.white, fontweight: FontWeight.w800, fontsize: 20,fontFamily: 'bold')),),
+                          SizedBox(height: 48,width: 255,child: ElevatedButton(onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context)=> BottomNavScreen()));}, style:ElevatedButton.styleFrom(backgroundColor: Color(0XFFE23744),shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),child: UiHelper.CustomText(text: 'Login', color: Colors.white, fontweight: FontWeight.w800, fontsize: 20,fontFamily: 'bold')),),
                           SizedBox(height: 8),
                           UiHelper.CustomText(text: 'Access your saved address from Luxury Mart automatically!', color: Color(0XFF9C9C9C), fontweight: FontWeight.normal, fontsize: 8),
                           SizedBox(height: 15),
                           UiHelper.CustomText(text: 'or login with phone number', color: Color(0XFF269237), fontweight: FontWeight.normal, fontsize: 14),
+
 
 
         
